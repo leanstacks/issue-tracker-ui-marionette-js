c2ed548c915b9b9852e8213543f080a5fd1789c8
Matt Warman <warmanmr@gmail.com>
2014-10-23 08:35:22 -0400

    Added support for the 'gulp' build system.

---
+++ gulpfile.js
@@ -0,0 +1,80 @@
+var gulp = require('gulp'),
+    jshint = require('gulp-jshint'),
+    jst = require('gulp-template-compile'),
+    concat = require('gulp-concat'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify'),
+    sourcemaps = require('gulp-sourcemaps'),
+    minifycss = require('gulp-minify-css'),
+    server = require('gulp-server-livereload'),
+    del = require('del');
+
+var config = {
+  version: "0.1.0"
+};
+
+gulp.task('clean', function(cb) {
+  del(['dist'], cb);
+});
+
+gulp.task('lib', function() {
+  return gulp.src('lib/**')
+    .pipe(gulp.dest('dist/assets/lib'));
+});
+
+gulp.task('scripts', function() {
+  return gulp.src('src/main/app/js/**/*.js')
+    .pipe(jshint.reporter('default'))
+    .pipe(sourcemaps.init())
+    .pipe(concat('app.js'))
+    .pipe(rename({ suffix: '-'}))
+    .pipe(rename({ suffix: config.version}))
+    .pipe(gulp.dest('dist/assets/app/js'))
+    .pipe(rename({ suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist/assets/app/js'));
+});
+
+gulp.task('templates', function() {
+  return gulp.src('src/main/app/templates/**/*.html')
+    .pipe(jst({ 
+       namespace: 'IssueTrackerTemplates',
+       name: function(file) { return file.relative.replace(/\.html$/,''); }
+     }))
+    .pipe(concat('app-templates.js'))
+    .pipe(rename({ suffix: '-'}))
+    .pipe(rename({ suffix: config.version}))
+    .pipe(gulp.dest('dist/assets/app/js'));
+});
+
+gulp.task('html', function() {
+  return gulp.src('src/main/app/*.html')
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('css', function() {
+  return gulp.src('src/main/app/css/**/*.css')
+    .pipe(gulp.dest('dist/assets/app/css'))
+    .pipe(rename({ suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('dist/assets/app/css'));
+});
+
+gulp.task('default', ['clean'], function() {
+  gulp.start('lib', 'templates', 'scripts', 'html', 'css');
+});
+
+gulp.task('run', ['lib', 'templates', 'scripts', 'html', 'css'], function() {
+  gulp.watch('src/main/app/templates/**/*.html', ['templates']);
+  gulp.watch('src/main/app/js/**/*.js', ['scripts']);
+  gulp.watch('src/main/app/*.html', ['html']);
+  gulp.watch('src/main/app/css/**/*.css', ['css']);
+
+  gulp.src('dist')
+    .pipe(server({
+      port: 9000,
+      livereload: true
+    }));
+});
+
